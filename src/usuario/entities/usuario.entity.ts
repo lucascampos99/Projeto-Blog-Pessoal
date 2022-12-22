3d5ec1b2f4fcd99ded64f7595c02c6c2f01d24d8
lucascampos99 <112710539+lucascampos99@users.noreply.github.com>
2022-12-22 08:22:19 -0300

    Add files via upload

---
+++ src/usuario/entities/usuario.entity.ts
@@ -0,0 +1,35 @@
+import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger"
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Postagem } from "../../postagem/entities/postagem.entity";
+
+@Entity({name: "tb_usuarios"})
+export class Usuario {
+
+    @PrimaryGeneratedColumn()
+    @ApiProperty()
+    id: number
+
+    @IsNotEmpty()
+    @Column({length: 255, nullable: false})
+    nome: string
+
+    @IsEmail()
+    @Column({length: 255, nullable: false})
+    @ApiProperty({example: "email@gmail.com.br"})
+    usuario: string //e-mail
+
+    @IsNotEmpty()
+    @MinLength(8)
+    @Column({length: 255, nullable: false})
+    @ApiProperty()
+    senha: string
+
+    @Column({length: 5000})
+    @ApiProperty()
+    foto: string
+
+    @ApiProperty({ type: () => Postagem})
+    @OneToMany(() => Postagem, (postagem) => postagem.usuario)
+    postagem: Postagem[]
+}
